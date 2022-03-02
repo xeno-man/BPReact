7ecff3f208d29b03b82381e798afa4f379835a63
Xeno Mannens <Xeno.mannens@me.com>
2022-03-02 21:35:52 +0100

    push

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="test" onPress={()=>{
+        console.log("test")
+        
+      }}/>
+      <Text>test</Text>
     </View>
   );
 }
